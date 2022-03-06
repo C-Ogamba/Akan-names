3334423c61f49de5b9d718a352e6ebdce700f1b8
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-03-06 15:32:38 +0300

    create arrays for the male and female names respectively and the specfic days

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const button = document.querySelector("#btn");
+const genderChoice = document.querySelectorAll("#gender");
+const showName = document.querySelector(".name");
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const maleAkanNames = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame",
+];
+
+const femaleAkanNames = [
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua",
+  "Ama",
+];
